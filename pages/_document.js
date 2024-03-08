0d601312af3e5c952bacdbdddd5473e0ff634c02
Ashiq Ilah <ashiqilah@Ashiqs-Mac-mini.local>
2024-03-08 12:24:38 +0530

    latest

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet"
